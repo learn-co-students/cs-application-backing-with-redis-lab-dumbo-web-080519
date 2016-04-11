6d944caaff51f093133f93db6b84d95f4e8e643e
Allen Downey <downey@allendowney.com>
2016-04-11 09:30:14 -0400

    Reading Redis URL from file

--- javacs-lab10/src/com/flatironschool/javacs/JedisMaker.java
+++ javacs-lab10/src/com/flatironschool/javacs/JedisMaker.java
@@ -1,6 +1,14 @@
 package com.flatironschool.javacs;
 
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import redis.clients.jedis.Jedis;
 
 
@@ -10,25 +18,81 @@ public class JedisMaker {
 	 * Make a Jedis object and authenticate it.
 	 * 
 	 * @return
+	 * @throws IOException 
 	 */
-	public static Jedis make() {
-		String host = "dory.redistogo.com";
-		int port = 10534;
-		String auth = System.getenv("REDISTOGO_AUTH");
+	public static Jedis make() throws IOException {
+		
+		
+		// assemble the directory name
+		String slash = File.separator;
+		String filename = System.getProperty("user.dir") + slash + 
+				"src" + slash + "resources" + slash + "redis_url.txt";
 
-		if (auth == null) {
-			System.out.println("To connect to RedisToGo, you have to create an environment");
-			System.out.println("variable named REDISTOGO_AUTH that contains your authorization");
-			System.out.println("code.  If you select and instance on the RedisToGo web page,");
-			System.out.println("you should see a URL that contains the information you need:");
-			System.out.println("redis://redistogo:AUTH_CODE@HOST:PORT");
-			System.out.println("In Eclipse, select Run->Run configurations...");
-			System.out.println("Open the Environment tab, and add a new variable.");
+	    StringBuilder sb = new StringBuilder();
+		BufferedReader br;
+		try {
+			br = new BufferedReader(new FileReader(filename));
+		} catch (FileNotFoundException e1) {
+			System.out.println("File not found: " + filename);
+			printInstructions();
+			return null;
 		}
+
+		while (true) {
+			String line = br.readLine();
+			if (line == null) break;
+			sb.append(line);
+		}
+		br.close();
+		
+		URI uri;
+		try {
+			uri = new URI(sb.toString());
+		} catch (URISyntaxException e) {
+			System.out.println("Reading file: " + filename);
+			System.out.println("It looks like this file does not contain a valid URI.");
+			printInstructions();
+			return null;
+		}
+		String host = uri.getHost();
+		int port = uri.getPort();
+		
+		String[] array = uri.getAuthority().split("[:@]");
+		String auth = array[1];
+		
+		//Here's an older version that read the auth code from an environment variable.
+		//String host = "dory.redistogo.com";
+		//int port = 10534;
+		//String auth = System.getenv("REDISTOGO_AUTH");
 		
 		Jedis jedis = new Jedis(host, port);
-		jedis.auth(auth);
+		
+		try {
+			jedis.auth(auth);
+		} catch (Exception e) {
+			System.out.println("Trying to connect to " + host);
+			System.out.println("on port " + port);
+			System.out.println("with authcode " + auth);
+			System.out.println("Got exception " + e);
+			printInstructions();
+			return null;
+		}
 		return jedis;
+	}
+
+
+	/**
+	 * 
+	 */
+	private static void printInstructions() {
+		System.out.println("");
+		System.out.println("To connect to RedisToGo, you have to provide a file called");
+		System.out.println("redis_url.txt that contains the URL of your Redis server.");
+		System.out.println("If you select an instance on the RedisToGo web page,");
+		System.out.println("you should see a URL that contains the information you need:");
+		System.out.println("redis://redistogo:AUTH@HOST:PORT");
+		System.out.println("Create a file called redis_url.txt in the src/resources");
+		System.out.println("directory, and paste in the URL.");
 	}
 
 
@@ -36,7 +100,7 @@ public class JedisMaker {
 	 * @param args
 	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
 		Jedis jedis = make();
 		
