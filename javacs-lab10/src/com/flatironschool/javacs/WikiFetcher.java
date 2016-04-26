69f570e7c31c7964be3a05ae957dbbcfad9bd42b
Charles Pletcher <cpletcher2@gmail.com>
2016-04-26 13:03:03 -0400

    Fix importing the redis_url.txt file

--- javacs-lab10/src/com/flatironschool/javacs/WikiFetcher.java
+++ javacs-lab10/src/com/flatironschool/javacs/WikiFetcher.java
@@ -18,21 +18,21 @@ public class WikiFetcher {
 
 	/**
 	 * Fetches and parses a URL string, returning a list of paragraph elements.
-	 * 
-	 * @param url 
+	 *
+	 * @param url
 	 * @return
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public Elements fetchWikipedia(String url) throws IOException {
 		sleepIfNeeded();
-		
+
 		// download and parse the document
 		Connection conn = Jsoup.connect(url);
 		Document doc = conn.get();
-		
+
 		// select the content text and pull out the paragraphs.
 		Element content = doc.getElementById("mw-content-text");
-		
+
 		// TODO: avoid selecting paragraphs from sidebars and boxouts
 		Elements paras = content.select("p");
 		return paras;
@@ -40,36 +40,36 @@ public class WikiFetcher {
 
 	/**
 	 * Reads the contents of a Wikipedia page from src/resources.
-	 * 
+	 *
 	 * @param url
 	 * @return
 	 * @throws IOException
 	 */
-	public Elements readWikipedia(String url) throws IOException {		
+	public Elements readWikipedia(String url) throws IOException {
 		URL realURL = new URL(url);
-		
+
 		// assemble the directory name
 		String slash = File.separator;
-		//String dirname = System.getProperty("user.dir") + slash + 
+		//String dirname = System.getProperty("user.dir") + slash +
 		//		"src" + slash + "resources" + slash + realURL.getHost();
 		//File input = new File(dirname, realURL.getPath());
 
 		String filename = "resources" + slash + realURL.getHost() + realURL.getPath();
-		
+
 		//System.out.println(filename);
 		InputStream stream = WikiFetcher.class.getClassLoader().getResourceAsStream(filename);
 		//System.out.println(stream);
 
 		// read the file
 		Document doc = Jsoup.parse(stream, "UTF-8", filename);
-		
+
 		// TODO: factor out the following repeated code
 		Element content = doc.getElementById("mw-content-text");
 		Elements paras = content.select("p");
 		return paras;
 	}
-	
-	/** 
+
+	/**
 	 * Rate limits by waiting at least the minimum interval between requests.
 	 */
 	private void sleepIfNeeded() {
@@ -87,12 +87,12 @@ public class WikiFetcher {
 		}
 		lastRequestTime = System.currentTimeMillis();
 	}
-	
+
 	public static void main(String[] args) throws IOException {
 		WikiFetcher wf = new WikiFetcher();
 		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
 		Elements paragraphs = wf.readWikipedia(url);
-		
+
 		for (Element paragraph: paragraphs) {
 			System.out.println(paragraph);
 		}
