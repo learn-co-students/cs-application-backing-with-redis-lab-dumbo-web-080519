975cec72ec7d682aa0e971e1cde05cbfe41f9b54
Charles Pletcher <cpletcher2@gmail.com>
2016-04-26 16:10:47 -0400

    Fix reading resources

--- javacs-lab10/src/com/flatironschool/javacs/WikiFetcher.java
+++ javacs-lab10/src/com/flatironschool/javacs/WikiFetcher.java
@@ -17,21 +17,21 @@ public class WikiFetcher {
 
 	/**
 	 * Fetches and parses a URL string, returning a list of paragraph elements.
-	 * 
-	 * @param url 
+	 *
+	 * @param url
 	 * @return
-	 * @throws IOException 
+	 * @throws IOException
 	 */
 	public Elements fetchWikipedia(String url) throws IOException {
 		sleepIfNeeded();
-		
+
 		// download and parse the document
 		Connection conn = Jsoup.connect(url);
 		Document doc = conn.get();
-		
+
 		// select the content text and pull out the paragraphs.
 		Element content = doc.getElementById("mw-content-text");
-		
+
 		// TODO: avoid selecting paragraphs from sidebars and boxouts
 		Elements paras = content.select("p");
 		return paras;
@@ -39,30 +39,29 @@ public class WikiFetcher {
 
 	/**
 	 * Reads the contents of a Wikipedia page from src/resources.
-	 * 
+	 *
 	 * @param url
 	 * @return
 	 * @throws IOException
 	 */
-	public Elements readWikipedia(String url) throws IOException {		
+	public Elements readWikipedia(String url) throws IOException {
 		URL realURL = new URL(url);
-		
-		// assemble the directory name
-		String slash = File.separator;
-		String dirname = System.getProperty("user.dir") + slash + 
-				"src" + slash + "resources" + slash + realURL.getHost();
 
-		// read the file
-		File input = new File(dirname, realURL.getPath());
-		Document doc = Jsoup.parse(input, "UTF-8", input.getName());
-		
+    // assemble the file name
+    String slash = File.separator;
+    String filename = "resources" + slash + realURL.getHost() + realURL.getPath();
+
+    // read the file
+    InputStream stream = WikiFetcher.class.getClassLoader().getResourceAsStream(filename);
+    Document doc = Jsoup.parse(stream, "UTF-8", filename);
+
 		// TODO: factor out the following repeated code
 		Element content = doc.getElementById("mw-content-text");
 		Elements paras = content.select("p");
 		return paras;
 	}
-	
-	/** 
+
+	/**
 	 * Rate limits by waiting at least the minimum interval between requests.
 	 */
 	private void sleepIfNeeded() {
